f16d6fca997b108ded9eb182778c36d76c109a32
TwistedModding <your-email@example.com>
2025-12-29 21:20:57 -0600

    regex fix for bulleted lists

--- src/components/sections/Changelog.js
+++ src/components/sections/Changelog.js
@@ -26,7 +26,7 @@ const Changelog = () => {
     html = html.replace(/(<li>.*<\/li>\n?)+/g, (match) => `<ul>${match}</ul>`);
     
     html = html.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
-    html = html.replace(/\*(.*?)\*/g, '<em>$1</em>');
+    html = html.replace(/\*([^\s*](?:.*?[^\s*])?)\*/g, '<em>$1</em>');
     html = html.replace(/`([^`]+)`/g, '<code>$1</code>');
     html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>');
     
